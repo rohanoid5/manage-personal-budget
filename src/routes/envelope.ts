0fd9d751563ca731ba824074165210aac0e798db
Rohan Dey <rohanoid5@gmail.com>
2021-05-15 18:20:45 +0530

    Added GET for single envelope

--- src/routes/envelope.ts
+++ src/routes/envelope.ts
@@ -3,8 +3,34 @@ import db from "../db";
 
 const envelopeRouter = express.Router();
 
+envelopeRouter.param("envelopeId", (req, res, next, id) => {
+  const envelope = db.envelope.getFromDBById(Number(id));
+
+  if (envelope) {
+    req.envelope = JSON.stringify(envelope);
+    next();
+  } else {
+    res.status(404).send({ message: "No envelope found" });
+  }
+});
+
 envelopeRouter.get("/", (req, res) => {
   res.send(db.envelope.getAllFromDB());
+});
+
+envelopeRouter.post("/", (req, res) => {
+  const body = req.body;
+
+  if (!body || !body.balance || !body.authorName || !body.envelopeName) {
+    return res.status(400).send({ message: "One or more properties missing" });
+  }
+
+  const newEnvelope = db.envelope.addToDB(body);
+  res.status(201).send(newEnvelope);
+});
+
+envelopeRouter.get("/:envelopeId", (req, res) => {
+  res.send(JSON.parse(req.envelope));
 });
 
 export default envelopeRouter;
---
+++ src/type/custom.d.ts
@@ -0,0 +1,5 @@
+declare namespace Express {
+  export interface Request {
+    envelope?: string;
+  }
+}
